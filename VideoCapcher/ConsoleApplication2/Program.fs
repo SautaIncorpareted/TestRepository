067cc1c107a0398dbc722f65125ad274eba4ad84
mmSAGmm <gasumta@gmail.com>
2014-01-19 14:05:00 -0800

    [*] Залид версию Женика видео капчера

---
+++ VideoCapcher/ConsoleApplication2/Program.fs
@@ -0,0 +1,26 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+open System
+
+[<EntryPoint>]
+let main (argv : string[]) =
+    //1 програмка
+    let greeting, thing = "some", "any"
+    let timeOfDay = System.DateTime.Now.ToString("hh.mm tt")
+    printfn "%s %s at %s" greeting thing timeOfDay
+    //2-я рограмка и первая функция
+
+    let printTruthTable f =
+        printfn "        |true   |false  |"
+        printfn "        +---------------+"
+        printfn "    true|%5b  |%5b  |" (f true true)(f true false)
+        printfn "   false|%5b  |%5b  |" (f false true)(f false false)
+        printfn "        +---------------+"
+        printfn ""
+          //();;
+    printTruthTable (&&)
+    printTruthTable (||)
+
+      
+    0 // возвращение целочисленного кода выхода
---
+++ VideoCapcher/VideoCore/VideoCapture.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using WebCam_Capture;
+namespace VideoCore
+{
+    public class VideoCapture
+    {
+        List<BitmapImage> images = new List<BitmapImage>();
+        private WebCamCapture webcam;
+        private System.Windows.Controls.Image _FrameImage;
+        private int FrameNumber = 5;
+        public void InitializeWebCam(ref System.Windows.Controls.Image ImageControl)
+        {
+            webcam = new WebCamCapture();
+            webcam.FrameNumber = ((ulong)(0ul));
+            webcam.TimeToCapture_milliseconds = FrameNumber;
+            webcam.ImageCaptured += new WebCamCapture.WebCamEventHandler(webcam_ImageCaptured);
+            _FrameImage = ImageControl;
+        }
+        public void FindMoving()
+        {
+            
+        }
+        
+        void webcam_ImageCaptured(object source, WebcamEventArgs e)
+        {
+            var image = Helper.LoadBitmap((System.Drawing.Bitmap)e.WebCamImage);
+            _FrameImage.Source = image;
+            int stride = image.PixelWidth * 4;
+            int size = image.PixelHeight * stride;
+            byte[] pixels = new byte[size];
+            image.CopyPixels(pixels, stride, 0);
+
+        }
+
+        public void Start()
+        {
+            webcam.TimeToCapture_milliseconds = FrameNumber;
+            webcam.Start(0);
+        }
+
+        public void Stop()
+        {
+            webcam.Stop();
+        }
+
+        public void Continue()
+        {
+            // change the capture time frame
+            webcam.TimeToCapture_milliseconds = FrameNumber;
+
+            // resume the video capture from the stop
+            webcam.Start(this.webcam.FrameNumber);
+        }
+
+        public void ResolutionSetting()
+        {
+            webcam.Config();
+        }
+
+        public void AdvanceSetting()
+        {
+            webcam.Config2();
+        }
+    }
+}
