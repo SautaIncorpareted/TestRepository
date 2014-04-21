9a2f4268033ff0962c3343a757599cdc8b788d9a
mmSAGmm <gasumta@gmail.com>
2014-04-21 22:34:38 +0300

    [*] Start gui

--- FTP_backaper/IOHelper/IOHelper.cs
+++ FTP_backaper/IOHelper/IOHelper.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace IOHelper
 {
@@ -24,6 +26,46 @@ namespace IOHelper
             var charList = byteList.Select(x => (char)x);
 
             return new string(charList.ToArray());
+        }
+
+        public static string GetPathToConfigs()
+        {
+            var location = Assembly.GetExecutingAssembly().Location;
+            var dirName = Path.GetDirectoryName(location);
+            var configPath = Path.Combine(dirName, "configs");
+            if (!Directory.Exists(configPath))
+            {
+                Directory.CreateDirectory(configPath);
+            }
+            return configPath;
+        }
+        public static void Serialize(object serializibleObject)
+        {
+            Serialize(serializibleObject, Path.Combine(GetPathToConfigs(), serializibleObject.GetType().Name));
+        }
+
+        public static void Serialize(object serializibleObject, string path)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, serializibleObject);
+            stream.Close();
+        }
+        public static T DeSerialize<T>(string path) where T : class, new()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            if (!File.Exists(path))
+            {
+                Serialize(new T());
+            }
+            Stream stream = new FileStream(path, FileMode.Open);
+            var obj = formatter.Deserialize(stream);
+            stream.Close();
+            return (T)obj;
+        }
+        public static T DeSerialize<T>() where T : class, new()
+        {
+            return DeSerialize<T>(Path.Combine(GetPathToConfigs(), (typeof(T)).Name));
         }
     }
 }
